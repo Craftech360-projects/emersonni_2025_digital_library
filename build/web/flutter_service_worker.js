c3894ab42e48d60c96eff7dce8437a0cac7d680f
abrahamcft360 <abraham@craftech360.com>
2025-04-02 13:17:53 +0530

    second commit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -58,7 +58,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3ba47a5a7ae08be0df2871f95f664708
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "acc0234d79121a7bbb2abd53111bfd8a",
+"flutter_bootstrap.js": "2824423d49c650047aff5754ce8d9ab1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
